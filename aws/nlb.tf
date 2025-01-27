2d89e74e82f3a60b426d8af9767ee15b6f40c62d
Britt Crawford <britt.v.crawford@gmail.com>
2025-01-27 12:36:43 -0800

    remove extraneous resources

--- aws/nlb.tf
+++ aws/nlb.tf
@@ -1,37 +1,39 @@
-resource "aws_lb" "director_nlb" {
-  name               = "${var.namespace}-director-nlb"
-  internal           = false
-  load_balancer_type = "network"
-  subnets            = var.subnet_ids
+# resource "aws_lb" "director_nlb" {
+#   name               = "${var.namespace}-director-nlb"
+#   internal           = false
+#   load_balancer_type = "network"
+#   subnets            = var.subnet_ids
 
-  tags = {
-    Name = "${var.namespace} Sandgarden Director NLB"
-  }
-}
+#   tags = {
+#     Name = "${var.namespace} Director NLB"
+#   }
+# }
 
-resource "aws_lb_target_group" "director_nlb_tg" {
-  name        = "${var.namespace}-director-tg"
-  port        = 8443
-  protocol    = "TCP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
+# resource "aws_lb_target_group" "director_nlb_tg" {
+#   name        = "${var.namespace}-director-tg"
+#   port        = 8987
+#   protocol    = "TCP"
+#   vpc_id      = var.vpc_id
+#   target_type = "ip"
 
-  health_check {
-    protocol            = "TCP"
-    port                = 8987
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    interval           = 30
-  }
-}
+#   health_check {
+#     protocol            = "HTTP"
+#     port                = 8987
+#     path               = "/health"
+#     healthy_threshold   = 3
+#     unhealthy_threshold = 3
+#     interval           = 30
+#     matcher            = "200-399"
+#   }
+# }
 
-resource "aws_lb_listener" "director_nlb" {
-  load_balancer_arn = aws_lb.director_nlb.arn
-  port              = 443
-  protocol          = "TCP"
+# resource "aws_lb_listener" "director_nlb" {
+#   load_balancer_arn = aws_lb.director_nlb.arn
+#   port              = 443
+#   protocol          = "TCP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.director_nlb_tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.director_nlb_tg.arn
+#   }
+# }
--- aws/secrets.tf
+++ aws/secrets.tf
@@ -13,17 +13,3 @@ resource "aws_secretsmanager_secret_version" "director" {
   secret_id = aws_secretsmanager_secret.director_api_key.id
   secret_string = var.sand_api_key
 }
-
-# SSM parameter for director version
-resource "aws_ssm_parameter" "director_version" {
-  name  = "/${var.namespace}/director/version"
-  type  = "String"
-  value = "latest"
-  lifecycle {
-    ignore_changes = [value]
-  }
-
-  tags = {
-    Name = "${var.namespace}-director-version"
-  }
-}

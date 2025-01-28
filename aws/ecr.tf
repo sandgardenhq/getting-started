7c34d2fce544f3a5ec50602897589a8b2b02eb3d
Britt Crawford <britt.v.crawford@gmail.com>
2025-01-27 17:28:37 -0800

    Enhance ECR and IAM configurations for Sandgarden Director
    - Add ECR pull-through cache rule and SSM parameter for repository URL
    - Create separate ECS task execution IAM role
    - Expand IAM policy to support ECR, Secrets Manager, and CloudWatch logging
    - Add VPC endpoints for ECR API and Docker registry
    - Update task definition to use new execution role and SSM parameter

--- aws/ecr.tf
+++ aws/ecr.tf
@@ -11,3 +11,17 @@ locals {
   ecr_repo_name    = split("/", aws_ecr_repository.ecr.repository_url)[1]
   ecr_registry_url = split("/", aws_ecr_repository.ecr.repository_url)[0]
 }
+
+resource "aws_ecr_pull_through_cache_rule" "public" {
+  ecr_repository_prefix = "sgdirector-cache"
+  upstream_registry_url = var.sandgarden_ecr_repo_url
+}
+
+# Update the repository URL in variables
+resource "aws_ssm_parameter" "ecr_repo_url" {
+  name  = "/${var.namespace}/ecr/repo_url"
+  type  = "String"
+  value = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/sgdirector-cache/h7j2x0j6/sgdirector:latest"
+}
+
+data "aws_caller_identity" "current" {}
--- aws/iam.tf
+++ aws/iam.tf
@@ -27,8 +27,13 @@ data "aws_iam_policy_document" "director_policy" {
     effect = "Allow"
     actions = [
       "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:ListSecrets"
     ]
-    resources = [aws_secretsmanager_secret.director_api_key.arn]
+    resources = [
+      aws_secretsmanager_secret.director_api_key.arn,
+      "${aws_secretsmanager_secret.director_api_key.arn}-??????"  # For secret versions
+    ]
   }
 
   statement {
@@ -65,6 +70,60 @@ data "aws_iam_policy_document" "director_policy" {
       values   = ["lambda.amazonaws.com"]
     }
   }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams",
+      "logs:GetLogEvents",
+      "logs:DescribeLogGroups"
+    ]
+    resources = [
+      "${aws_cloudwatch_log_group.director.arn}:*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetRepositoryPolicy",
+      "ecr:SetRepositoryPolicy"
+    ]
+    resources = [
+      aws_ecr_repository.ecr.arn,
+      "${aws_ecr_repository.ecr.arn}/*",
+      "arn:aws:ecr:${var.aws_region}:${data.aws_caller_identity.current.account_id}:repository/sgdirector-cache/*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:GetServiceBearerToken"
+    ]
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_policy" "director_policy" {
@@ -86,7 +145,59 @@ resource "aws_iam_role_policy_attachment" "director_policy_attachment" {
   policy_arn = aws_iam_policy.director_policy.arn
 }
 
+resource "aws_iam_role_policy_attachment" "execution_role_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.director_policy.arn
+}
+
 resource "aws_iam_role_policy_attachment" "director_managed_core" {
   role       = aws_iam_role.director_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+# Create separate execution role for ECS tasks
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.namespace}-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach AWS managed policy for ECS task execution
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Add a specific policy for secrets access to the execution role
+resource "aws_iam_role_policy" "execution_secrets_policy" {
+  name = "${var.namespace}-execution-secrets-policy"
+  role = aws_iam_role.ecs_task_execution_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ]
+        Resource = [
+          aws_secretsmanager_secret.director_api_key.arn,
+          "${aws_secretsmanager_secret.director_api_key.arn}-??????"
+        ]
+      }
+    ]
+  })
 }

1c0c0ad524ba28119a1a7ac4a2eb574c3a8376f9
Britt Crawford <britt.v.crawford@gmail.com>
2025-01-29 09:25:41 -0800

    Add sandgarden CLI to dev container setup

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Hi 👋
+# You don't need to edit or run this script.
+# It's just here to make sure the devcontainer is set up correctly.
+
+echo "Setting up devcontainer"
+echo "Downloading Sandgarden CLI..."
+curl https://api.sandgarden.com/api/v1/assets/sand/latest/sand_linux_amd64 -L -o /usr/local/bin/sand
+sudo chmod 0755 /usr/local/bin/sand
+sudo chown -R vscode:vscode /workspaces/sandgarden
+echo "Done"

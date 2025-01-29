533caa4d3b57e966787a1bc16a967971a8e9c4ea
Britt Crawford <britt.v.crawford@gmail.com>
2025-01-28 19:42:30 -0800

    Add Network Load Balancer security group and ingress rules
    - Create dedicated security group for Network Load Balancer
    - Add HTTPS inbound rule allowing traffic from anywhere
    - Configure security group ingress rule for NLB to ECS traffic
    - Attach security group to Network Load Balancer

--- aws/nlb.tf
+++ aws/nlb.tf
@@ -3,6 +3,7 @@ resource "aws_lb" "director_nlb" {
   internal           = false
   load_balancer_type = "network"
   subnets            = [aws_subnet.private.id]
+  security_groups    = [aws_security_group.nlb_sg.id]
 
   tags = {
     Name = "${var.namespace} Director NLB"
@@ -34,4 +35,35 @@ resource "aws_lb_listener" "director_nlb" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.director_nlb_tg.arn
   }
+}
+
+# Security group for NLB
+resource "aws_security_group" "nlb_sg" {
+  name        = "${var.namespace}-nlb-sg-2"
+  description = "Security group for Network Load Balancer"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.namespace}-nlb-sg"
+  }
+}
+
+# Allow inbound HTTPS to NLB
+resource "aws_vpc_security_group_ingress_rule" "nlb_https" {
+  security_group_id = aws_security_group.nlb_sg.id
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "Allow HTTPS inbound"
+}
+
+# Allow NLB to ECS traffic
+resource "aws_vpc_security_group_ingress_rule" "ecs_from_nlb" {
+  security_group_id = aws_security_group.sandgarden_director_sg.id
+  from_port         = 8987
+  to_port           = 8987
+  ip_protocol       = "tcp"
+  description       = "Allow traffic from NLB"
+  referenced_security_group_id = aws_security_group.nlb_sg.id
 }

6cde8f9a2ce800448ddd371e7026c55517d2bcb3
Patrick Stephen <patrick.stephen@sandgarden.com>
2025-02-04 11:25:34 -0600

    aws/vpc: update to use ec2+ecs

--- aws/vpc/variables.tf
+++ aws/vpc/variables.tf
@@ -40,16 +40,16 @@ variable "sandgarden_ecr_repo_url" {
   default     = "public.ecr.aws"  # Changed to just the registry domain
 }
 
-variable "fargate_cpu" {
-  description = "Fargate instance CPU units (1 vCPU = 1024 CPU units)"
+variable "task_cpu" {
+  description = "ECS Task instance CPU units (1 vCPU = 1024 CPU units)"
   type        = number
   default     = 1024
 }
 
-variable "fargate_memory" {
-  description = "Fargate instance memory in MiB"
+variable "task_memory" {
+  description = "ECS Task instance memory in MiB"
   type        = number
-  default     = 2048
+  default     = 970
 }
 
 variable "director_version" {

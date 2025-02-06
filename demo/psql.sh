5a9d3bfe3111e2f82dedc4b2790e01335a3564dc
Andy Magnusson <andy@sandgarden.com>
2025-02-06 13:40:49 +0000

    updates to psql wrapper

--- demo/psql.sh
+++ demo/psql.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-SCRIPT_DIR="$( dirname -- "${BASH_SOURCE[0]}" )"
-source "${SCRIPT_DIR}/_load_env.sh"
-load_env
+export DB_HOST="postgres"
+export DB_PORT="5432"
+export DB_NAME="tickets"
+export DB_USER="tickets"
+export DB_PASS="devpassword"
 
 SQL=$1
 
-NET_OPT=""
-if [[ "${DEMO_TYPE}" == "docker" ]]; then
-  NET_OPT="--net=sandgarden_demo_network"
-fi
+NET_OPT="--net=sandgarden_demo_network"
 
 # psql via docker -it with PGPASSWORD environment variable
 docker run $NET_OPT -t --rm -e PGPASSWORD=$DB_PASS postgres psql -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER --pset pager=off -c "$SQL"
